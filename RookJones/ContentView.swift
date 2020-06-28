a72b9b3b8b3979ad1d112c02589245edd3424ed3
Richard Pineo <Richard.E.Pineo@ge.com>
2020-06-28 10:35:40 -0700

    Initial Commit

---
+++ RookJones/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RookJones
+//
+//  Created by Richard Pineo on 6/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
